ae1aea18a00a93b3d51d8d5761f89a757b788f5c
Marcello Magno <marcello_magno@hotmail.com>
2025-02-18 16:17:23 -0300

    feat: Implementing connection in db and repository

--- src/model/entities/eventos.py
+++ src/model/entities/eventos.py
@@ -5,5 +5,5 @@ class Eventos(Base):
     __tablename__ = "Eventos"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False)
+    nome = Column(String, nullable=False)
     
